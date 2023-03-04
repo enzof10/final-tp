31194b436ffd7cd5f0a58f3f9bf8098b824a2eee
enzof10 <enzo_17_05@hotmail.com>
2023-03-04 18:27:09 -0300

    finish validation auth

--- front-flask/app.py
+++ front-flask/app.py
@@ -11,6 +11,8 @@ def main():
 @app.route("/sign-in", methods=['POST', "GET"])
 def sign_in():
     error = ''
+    if("user" in session):
+        return redirect('artists', code=302)
     if(request.method == "POST"):
         userName = request.form['username']
         if userName:
@@ -23,6 +25,8 @@ def sign_in():
             error = resp["isValid"] 
             if(resp["isValid"]):
                 session["user"] = resp["user"]["Username"]
+                print("session[user]")
+                print(session["user"])
                 return redirect('artists',code=302)
     return render_template('signin.html', errorLogin = error)
 
@@ -36,42 +40,46 @@ def sign_up():
     auth_data = {'username': userName, 'password': password, 'fullname' : fullname}
     resp = requests.post('http://localhost:8000/sign-up', data=auth_data)
     resp = resp.json() 
-    print("resp sign-up")
-    print(resp)
     error = resp["message"]
     if(not resp["error"]):
         session["user"] = resp["user"]["name"]
-        return redirect('artists.html', code=302)
+        return redirect('artists', code=302)
     return render_template('signin.html', errorLogin = error)
 
 
 @app.route("/artists", methods=['GET'])
 def artists():
-    artistName = request.args.get('search')
-    if(artistName != "" and artistName):
-        artists = requests.get('http://localhost:8000/artists' + "?search=" + artistName)
+    if("user" in session):
+        artistName = request.args.get('search')
+        if(artistName != "" and artistName):
+            artists = requests.get('http://localhost:8000/artists' + "?search=" + artistName)
+        else:
+            artists = requests.get('http://localhost:8000/artists')
+        artists = artists.json()
+        return render_template('artists.html', artists = artists)
     else:
-        artists = requests.get('http://localhost:8000/artists')
-    artists = artists.json()
-    return render_template('artists.html', artists = artists)
+        return redirect('sign-in', code=302)
+
 
 @app.route("/artists/<name>/edit", methods=['GET'])
 def edit_artist(name):
-    print(name)
-    artist = requests.get('http://localhost:8000/artists' + "?search=" + name)
-    artist = artist.json()
-    print(artist[0])
-    return render_template('edit-artist.html', artist = artist[0])
+    if("user" in session):
+        artist = requests.get('http://localhost:8000/artists' + "?search=" + name)
+        artist = artist.json()
+        return render_template('edit-artist.html', artist = artist[0])
+    else:
+        return redirect('http://localhost:5000/sign-in', code=302)
 
 
 @app.route("/artists/<artisID>/albums", methods=['GET'])
 def artist_songs(artisID):
-    print(artisID)
-    artistName = request.args.get('artisName')
-    albums = requests.get('http://localhost:8000/artists/' + artisID +  "/albums")
-    albums = albums.json()
-    print(albums)
-    return render_template('artist_albums.html', albums = albums, artistName = artistName )
+    if("user" in session):
+        artistName = request.args.get('artisName')
+        albums = requests.get('http://localhost:8000/artists/' + artisID +  "/albums")
+        albums = albums.json()
+        return render_template('artist_albums.html', albums = albums, artistName = artistName )
+    else:
+        return redirect('http://localhost:5000/sign-in', code=302)
 
 
 if __name__ == '__main__':
